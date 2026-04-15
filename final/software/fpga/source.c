4223ab76ce4296485cf1431cfba012185d63a193
tiesen243 <ttien56906@gmail.com>
2026-04-15 20:33:22 +0700

    refactor: enhance UART data handling and update matrix text scrolling logic

--- final/software/fpga/source.c
+++ final/software/fpga/source.c
@@ -61,19 +61,91 @@ static int parse_fixed_digits(const char *s, int len);
 static int parse_uart_time_command(const char *cmd, Date *out);
 void send_matrix_led(uint8_t address, uint8_t data);
 
-static void update_matrix_dm_scroll(void)
+static uint8_t get_font5x7_row(char ch, int row)
 {
-  static int scroll_pos = -12;
+  if (row < 0 || row > 6)
+    return 0;
+
+  switch (ch)
+  {
+  case 'I':
+    switch (row)
+    {
+    case 0:
+    case 6:
+      return 0x1F;
+    default:
+      return 0x04;
+    }
+  case 'L':
+    return (row == 6) ? 0x1F : 0x10;
+  case 'O':
+    switch (row)
+    {
+    case 0:
+    case 6:
+      return 0x0E;
+    default:
+      return 0x11;
+    }
+  case 'V':
+    switch (row)
+    {
+    case 5:
+      return 0x0A;
+    case 6:
+      return 0x04;
+    default:
+      return 0x11;
+    }
+  case 'E':
+    switch (row)
+    {
+    case 0:
+    case 3:
+    case 6:
+      return 0x1F;
+    default:
+      return 0x10;
+    }
+  case 'U':
+    return (row == 6) ? 0x0E : 0x11;
+  case 'H':
+    return (row == 3) ? 0x1F : 0x11;
+  default:
+    return 0x00;
+  }
+}
+
+static int get_text_pixel(const char *text, int text_len, int row, int text_col)
+{
+  int cell_width = 6;
+  int ch_index;
+  int col_in_cell;
+  uint8_t row_bits;
+
+  if (row < 0 || row > 6 || text_col < 0)
+    return 0;
+
+  ch_index = text_col / cell_width;
+  if (ch_index >= text_len)
+    return 0;
+
+  col_in_cell = text_col % cell_width;
+  if (col_in_cell >= 5)
+    return 0;
+
+  row_bits = get_font5x7_row(text[ch_index], row);
+  return ((row_bits >> (4 - col_in_cell)) & 0x01);
+}
+
+static void update_matrix_text_scroll(void)
+{
+  static int scroll_pos = 8;
   static int tick_divider = 0;
-  static const char dm_pattern[8][13] = {
-      "011100100010",
-      "010010110110",
-      "010010101010",
-      "010010101010",
-      "010010100010",
-      "010010100010",
-      "011100100010",
-      "000000000000"};
+  static const char text[] = "I LOVE IUH";
+  const int text_len = (int)strlen(text);
+  const int text_width = text_len * 6 - 1;
   int row, col;
 
   tick_divider++;
@@ -89,16 +161,16 @@ static void update_matrix_dm_scroll(void)
     {
       int text_col = col - scroll_pos;
 
-      if (text_col >= 0 && text_col < 12 && dm_pattern[row][text_col] == '1')
+      if (get_text_pixel(text, text_len, row, text_col))
         row_data |= (uint8_t)(1u << (7 - col));
     }
 
     send_matrix_led((uint8_t)(row + 1), row_data);
   }
 
-  scroll_pos++;
-  if (scroll_pos > 8)
-    scroll_pos = -12;
+  scroll_pos--;
+  if (scroll_pos < -text_width)
+    scroll_pos = 8;
 }
 
 void Timer_IQR_Handler(void *isr_context)
@@ -135,7 +207,7 @@ void Timer_IQR_Handler(void *isr_context)
     IOWR(BUZZ_BASE, 0, 0);
   }
 
-  update_matrix_dm_scroll();
+  update_matrix_text_scroll();
 
   timer_clear_timeout();
 }
@@ -229,6 +301,9 @@ int main(void)
       alarm_counter = 1000;
       uart_send_string("A1");
       IOWR(LED_BASE, 0, 1);
+      uart_send_string("A1");
+      uart_send_string("A1");
+      uart_send_string("A1");
     }
 
     // UART handler
@@ -303,7 +378,6 @@ int main(void)
         mode = RUNNING;
       }
     }
-
   }
 
   return 0;
