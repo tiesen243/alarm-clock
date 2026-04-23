f74ea11498d4e7d497ec48df0606b6a58baaa2bf
tiesen243 <ttien56906@gmail.com>
2026-04-23 19:44:04 +0700

    chore: minor changes

--- software/fpga/include/utils.h
+++ software/fpga/include/utils.h
@@ -3,6 +3,25 @@
 
 #include "datetime.h"
 
+const int HEX_7SEG[16] = {
+    0x40, // 0
+    0x79, // 1
+    0x24, // 2
+    0x30, // 3
+    0x19, // 4
+    0x12, // 5
+    0x02, // 6
+    0x78, // 7
+    0x00, // 8
+    0x10, // 9
+    0x08, // A
+    0x03, // b
+    0x46, // C
+    0x21, // d
+    0x06, // E
+    0x0E  // F
+};
+
 int run_set_datetime(Date *target, const char *done_label);
 int parse_uart_time_command(const char *cmd, Date *out);
 
